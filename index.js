85db034ada0f07ec9e0c80f4c1ec08d54bd58cd5
GRboy <dlekwns@naver.com>
2021-05-22 23:24:55 +0900

    practice js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("ul li a").style.color = "red";
+document.querySelector("button").style.background ="yellow";
+
+// js로 스타일 직접 바꾸는것은 디버깅에 좋지않음
+document.querySelector("h1").classList.toggle("huge-text");
